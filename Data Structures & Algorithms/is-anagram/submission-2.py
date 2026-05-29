4814b3846cc8fc6e20d330fbaec9079ada9d00f0
Tomas SL <tomas.sl@hotmail.com>
2026-05-29 19:55:56 -0300

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        count_s = Counter(s)
+        count_t = Counter(t)
+
+        if count_s == count_t:
+            return True
+        return False
